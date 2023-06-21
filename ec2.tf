34da60f1ad45ea62a357b0bc0c0d7c5243c6bd3b
VinayKumar <1409vinaykumar@gmail.com>
2023-06-21 19:21:58 +0530

    mod

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAXQ3YWTRJHA7QTAXO"
-    secret_key = "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"
+    access_key = "AKIAXQ3YWTRJMROUN2O4"
+    secret_key = "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"
     region = "ap-south-1"
 }
 
@@ -8,5 +8,4 @@ resource "aws_instance" "ec2_instance" {
     ami = "ami-057752b3f1d6c4d6c"
     instance_type = "t2.micro"
     key_name = "vinaws2"
-    security_groups = ["devopspractice"]
 } 
