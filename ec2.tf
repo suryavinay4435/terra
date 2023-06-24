d7967f661c849906ee1004ac4ec19001c067ad22
VinayKumar <1409vinaykumar@gmail.com>
2023-06-24 10:28:57 +0530

    terraf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    access_key = "AKIAXQ3YWTRJAAIHKHHY"
-    secret_key = "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"
+    access_key = "AKIAXQ3YWTRJG3QQI7ND"
+    secret_key = "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"
     region = "us-east-1"
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-0d668681c4b8de28c"
+    ami = "ami-057752b3f1d6c4d6c"
     instance_type = "t2.micro"
     key_name = "vinaws2"
 } 
